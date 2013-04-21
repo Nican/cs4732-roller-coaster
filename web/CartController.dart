608e4b6b1a510b790be3d39cb4fff951acaeb474
Andrew Feeney <andrew.feeney@wpi.edu>
2013-04-21 19:59:08 -0400

    Drag
    added drag and switching direction on hills when out of momentum

--- web/CartController.dart
+++ web/CartController.dart
@@ -5,13 +5,19 @@ class CartController{
   Curve3D curve;
   num totalDist;
   num traveledDist;
-  num maxHeight;
+  num Energy;
   num curHeight;
   num g;
-  CartController(this.curve, [this.maxHeight = 200, this.g = 0.1]){
+  num drag;
+  bool forward;
+  num lastVelocity;
+  
+  CartController(this.curve, [this.Energy = 20, this.g = 0.1, this.drag =0.0001]){
     totalDist = curve.getLengths().last;
     traveledDist = 0;
     curHeight = curve.getPoint(0).y;
+    forward = true;
+    lastVelocity = 0;
   }
   
   Vector3 getPoint(num distance){
@@ -20,11 +26,33 @@ class CartController{
   
   Vector3 getNextPoint(num t_delta){
     num velocity = Math.sqrt(Math.max(2*g*(maxHeight-curHeight),0));
+    print(velocity);
+    if(velocity == 0){
+      forward = !forward;
+      if(getPoint(traveledDist - lastVelocity).y != curHeight){
+        velocity = lastVelocity;
+      }
+    }
+    print("\t"+velocity.toString());
     num distance = velocity * (t_delta);
-    traveledDist += distance;
+    
+    
+    if(forward){
+      traveledDist += distance;
+    }
+    else{
+      traveledDist -= distance;
+    }
+    
     Vector3 next = getPoint(traveledDist);
     curHeight = next.y;
+    lastVelocity = velocity;
+    
+    Energy = Energy - drag*velocity*distance;
+    
     return next;
   }
+  
+  num get maxHeight => Energy/g;
 }
 
