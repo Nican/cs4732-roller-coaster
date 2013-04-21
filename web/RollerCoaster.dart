6f849ab0e420091bd05922b7302fbbc939a4e430
Nican <nican132@gmail.com>
2013-04-21 19:05:10 -0400

    Added basic plane of the RollerCoaster.

--- web/RollerCoaster.dart
+++ web/RollerCoaster.dart
@@ -2,6 +2,43 @@ part of RollerCoaster;
 
 class RollerCoaster extends Geometry {
   
+  RollerCoaster( SplineCurve3 curve ) : super() {
+    
+    num delta = 0.01;
+    
+    for(num t = 0; t <= 1.0; t += delta ){
+      addSegment( curve.getPoint(t), curve.getPoint(t+delta), curve.getPoint((t+delta*2)%1) );
+    }
+    
+    
+    computeCentroids();
+    mergeVertices();
+  }
   
-  
+  void addSegment( Vector3 position, Vector3 next, Vector3 next2 )
+  {
+    Vector3 normal = new Vector3().sub(next, position).normalize();
+    Vector3 cross = new Vector3().cross( normal, new Vector3(0,1,0) ).normalize().multiplyScalar(10);
+    
+    Vector3 normal2 = new Vector3().sub(next2, next).normalize();
+    Vector3 cross2 = new Vector3().cross( normal2, new Vector3(0,1,0)  ).normalize().multiplyScalar(10);
+    var offset = vertices.length;
+    
+    vertices.add( new Vector3().add(position, cross) );
+    vertices.add( new Vector3().sub(position, cross) );
+    vertices.add( new Vector3().add(next, cross2) );
+    vertices.add( new Vector3().sub(next, cross2) );
+    
+    faces.add( new Face4( 0 + offset, 2 + offset, 3 + offset, 1 + offset ) );
+    
+    List faceVertexUV = faceVertexUvs[ 0 ];
+    faceVertexUV.add( [
+      new UV( 0, 0 ),
+      new UV( 0, 1 ),
+      new UV( 1, 1 ),
+      new UV( 1, 0 ),
+    ] );
+    
+    
+  }
 }
