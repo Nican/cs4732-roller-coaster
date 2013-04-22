adf6fbb76d826948514ef15ff0ddd8400ef68150
Nican <nican132@gmail.com>
2013-04-22 15:42:10 -0400

    Removed unnecessary code.

--- web/webgltest.dart
+++ web/webgltest.dart
@@ -12,12 +12,11 @@ class Canvas_Geometry_Cube
 {
   Element container;
 
-  PerspectiveCamera camera;
-  Scene scene;
+  PerspectiveCamera camera = new PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 10000 );
+  Scene scene = new Scene();
   CanvasRenderer renderer;
 
   Mesh cube;
-  Mesh plane;
 
   num targetRotation;
   num targetRotationOnMouseDown;
@@ -67,21 +66,8 @@ class Canvas_Geometry_Cube
     windowHalfY = window.innerHeight / 2;
 
     container = new Element.tag('div');
-    //document.body.appendChild( container );
     document.body.nodes.add( container );
 
-    Element info = new Element.tag('div');
-    info.style.position = 'absolute';
-    info.style.top = '10px';
-    info.style.width = '100%';
-    info.style.textAlign = 'center';
-    info.innerHtml = 'Drag to spin the cube';
-    //container.appendChild( info );
-    container.nodes.add( info );
-
-    scene = new Scene();
-
-    camera = new PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 10000 );
     camera.position.y = 150;
     camera.position.z = 500;
     scene.add( camera );
@@ -104,99 +90,24 @@ class Canvas_Geometry_Cube
     scene.add(coaster);
     
 
-    // Plane
-
-    plane = new Mesh( new PlaneGeometry( 200, 200 ), new MeshBasicMaterial( color: 0xe0e0e0, overdraw: true ) );
-    plane.rotation.x = - 90 * ( Math.PI / 180 );
-    //plane.overdraw = true; //TODO where is this prop?
-    //scene.add( plane );
-
+    // Renderer
     renderer = new CanvasRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
 
-
-    //container.appendChild( renderer.domElement );
-    container.nodes.add( renderer.domElement );
-
-    document.onMouseDown.listen(onDocumentMouseDown);
-    document.onTouchStart.listen(onDocumentTouchStart);
-    document.onTouchMove.listen(onDocumentTouchMove);
-  
-    //new Timer.periodic(new Duration(milliseconds:10), (Timer timer) => animate());
-    
+     container.nodes.add( renderer.domElement );
   }
-
-  void onDocumentMouseDown( event )
-  {
-    event.preventDefault();
-
-    document.onMouseMove.listen(onDocumentMouseMove);
-    document.onMouseUp.listen(onDocumentMouseUp);
-    document.onMouseOut.listen(onDocumentMouseOut);
-
-    mouseXOnMouseDown = event.clientX - windowHalfX;
-    targetRotationOnMouseDown = targetRotation;
-  }
-
-  void onDocumentMouseMove( event )
-  {
-    mouseX = event.clientX - windowHalfX;
-
-    targetRotation = targetRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.02;
-  }
-
-  void onDocumentMouseUp( event )
-  {
-    //document.onMouseMove.remove(onDocumentMouseMove);
-    //document.onMouseUp.remove(onDocumentMouseUp);
-    //document.onMouseOut.remove(onDocumentMouseOut);
-  }
-
-  void onDocumentMouseOut( event )
-  {
-    //document.onMouseMove.remove(onDocumentMouseMove);
-    //document.onMouseUp.remove(onDocumentMouseUp);
-    //document.onMouseOut.remove(onDocumentMouseOut);
-  }
-
-  void onDocumentTouchStart( event )
-  {
-    if ( event.touches.length == 1 )
-    {
-      event.preventDefault();
-
-      mouseXOnMouseDown = event.touches[ 0 ].pageX - windowHalfX;
-      targetRotationOnMouseDown = targetRotation;
-    }
-  }
-
-  void onDocumentTouchMove( event )
-  {
-    if ( event.touches.length == 1 )
-    {
-      event.preventDefault();
-
-      mouseX = event.touches[ 0 ].pageX - windowHalfX;
-      targetRotation = targetRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.05;
-    }
-  }
-
 
   void animate(num highResTime)
   {
-//    window.webkitRequestAnimationFrame(animate);
-//    print('win dynamic ${window.dynamic['requestAnimationFrame']}');
-//    if ( window.dynamic['requestAnimationFrame'] != null )
-//      window.dynamic['requestAnimationFrame']( animate );
-
     render(highResTime);
     window.requestAnimationFrame(animate);
   }
   
   num lastTime = 0;
-  void render(t)
+  void render(num t)
   {
-    cube.position = cc.getNextPoint(t-lastTime);
+    num delta = Math.min(t-lastTime, 100.0);
+    cube.position = cc.getNextPoint(delta);
     cube.position.y += 50;
 
     renderer.render( scene, camera );
