6016cb21c91d51ef612dc48e03b632c8f4a05df6
Andrew Feeney <andrew.feeney@wpi.edu>
2013-04-28 21:14:11 -0400

    Added particle system to CoasterRider

--- web/CoasterRider.dart
+++ web/CoasterRider.dart
@@ -5,21 +5,27 @@ class CoasterRider implements GameState  {
   SpiderCoaster coaster;
   CartController cc;
   Mesh cube;
+  SparkParticleHandler psystem;
   
   CoasterRider(SpiderCoaster this.coaster){
     // Cube
     cube = new Mesh( new CubeGeometry( 20, 20, 20, 1, 1, 1 ), coaster.normalMaterial );
+    psystem = new SparkParticleHandler(250, coaster.scene, 20, 10, false);
   }
   
   void begin()
   {
     cc = new CartController(coaster.spline, .16, .001);
     coaster.scene.add( cube );
+    psystem.add();
+    psystem.active = true;
   }
   
   void end()
   {
     coaster.scene.remove( cube );
+    psystem.remove();
+    psystem.active = false;
   }
   
   void update(num delta)
@@ -35,9 +41,14 @@ class CoasterRider implements GameState  {
     cube.rotation.setEulerFromQuaternion(quaternion);
     cube.position.addSelf( quaternion.multiplyVector3(new Vector3(0,1,0)).multiplyScalar(18) );
     
-    coaster.camera.position = cube.position;
-    coaster.camera.rotation = cube.rotation;
+    psystem.active = cc.forces.x.abs()>2;
+    psystem.position = cube.position.clone().addSelf(quaternion.multiplyVector3(new Vector3(cc.forces.x.isNegative?20:-10,-10,cc.forward?-10:10)));
+    psystem.forward = quaternion.multiplyVector3(new Vector3(cc.forces.x.isNegative?1:-1,0,0));
+    psystem.velocity = forward.clone().multiplyScalar(cc.calcSpeed());
+    psystem.update();
     
+    coaster.camera.position = cube.position;
+    coaster.camera.rotation = cube.rotation;   
   }
   
 }
