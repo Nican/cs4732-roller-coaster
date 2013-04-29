95c19bc67092709e96a601ce18408c3d035142f2
Andrew Feeney <andrew.feeney@wpi.edu>
2013-04-28 22:45:20 -0400

    Third Person View for Cart Rider

--- web/CoasterRider.dart
+++ web/CoasterRider.dart
@@ -6,12 +6,19 @@ class CoasterRider implements GameState  {
   CartController cc;
   Mesh cube;
   SparkParticleHandler psystem;
+  bool thirdPerson = false;
+  
+  StreamSubscription<KeyboardEvent> keyDownEvent;
+  FirstPersonControls controls;
   
   CoasterRider(SpiderCoaster this.coaster){
     // Cube
     cube = new Mesh( new CubeGeometry( 20, 20, 20, 1, 1, 1 ), coaster.normalMaterial );
     cube.useQuaternion = true;
     psystem = new SparkParticleHandler(250, coaster.scene, 20, 10, false);
+    keyDownEvent = document.body.onKeyDown.listen(keyHandler);
+    keyDownEvent.pause();
+    controls = new FirstPersonControls (coaster.camera, document.body);
   }
   
   void begin()
@@ -21,6 +28,8 @@ class CoasterRider implements GameState  {
     coaster.camera.useQuaternion = true;
     psystem.add();
     psystem.active = true;
+    keyDownEvent.resume();
+    thirdPerson = false;
   }
   
   void end()
@@ -29,6 +38,7 @@ class CoasterRider implements GameState  {
     coaster.camera.useQuaternion = false;
     psystem.remove();
     psystem.active = false;
+    keyDownEvent.pause();
   }
   
   void update(num delta)
@@ -42,8 +52,13 @@ class CoasterRider implements GameState  {
     cube.quaternion = quaternion;
     cube.position.addSelf( quaternion.multiplyVector3(new Vector3(0,1,0)).multiplyScalar(18) );
 
-    coaster.camera.position = cube.position;
-    coaster.camera.quaternion = cube.quaternion;
+    if(!thirdPerson){
+      coaster.camera.position = cube.position;
+      coaster.camera.quaternion = cube.quaternion;
+    }
+    else{
+      controls.update(delta);
+    }
 
     psystem.active = cc.forces.x.abs()>2;
     psystem.position = cube.position.clone().addSelf(quaternion.multiplyVector3(new Vector3(cc.forces.x.isNegative?20:-10,-10,cc.forward?-10:10)));
@@ -51,5 +66,10 @@ class CoasterRider implements GameState  {
     psystem.velocity = forward.clone().multiplyScalar(cc.calcSpeed());
     psystem.update();
   }
-  
+  void keyHandler(KeyboardEvent e){
+    if(e.keyCode == KeyCode.T){
+      thirdPerson = !thirdPerson;
+      coaster.camera.useQuaternion = !coaster.camera.useQuaternion;
+    }
+  }
 }
