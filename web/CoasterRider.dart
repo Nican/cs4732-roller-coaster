cda390b61d78ac2c4ea3ae63222e028d76d2b09b
Nican <nican132@gmail.com>
2013-04-28 21:30:56 -0400

    Using quaternions for rotation.
    It is easier than going back and forth.

--- web/CoasterRider.dart
+++ web/CoasterRider.dart
@@ -9,17 +9,20 @@ class CoasterRider implements GameState  {
   CoasterRider(SpiderCoaster this.coaster){
     // Cube
     cube = new Mesh( new CubeGeometry( 20, 20, 20, 1, 1, 1 ), coaster.normalMaterial );
+    cube.useQuaternion = true;
   }
   
   void begin()
   {
     cc = new CartController(coaster.spline, .16, .001);
     coaster.scene.add( cube );
+    coaster.camera.useQuaternion = true;
   }
   
   void end()
   {
     coaster.scene.remove( cube );
+    coaster.camera.useQuaternion = false;
   }
   
   void update(num delta)
@@ -28,15 +31,13 @@ class CoasterRider implements GameState  {
     cube.position = cc.getCurPoint();
     
     Vector3 forward = coaster.spline.getForward(cc.cur_t);        
-    Quaternion quaternion = coaster.spline.getQuaternion(cc.cur_t);    
-    Quaternion quaternion2 = new Quaternion().rotationBetween(new Vector3(0,0,-1), forward );
-    quaternion.multiplySelf( quaternion2 );
+    Quaternion quaternion = coaster.spline.getQuaternion2(cc.cur_t);  
     
-    cube.rotation.setEulerFromQuaternion(quaternion);
+    cube.quaternion = quaternion;
     cube.position.addSelf( quaternion.multiplyVector3(new Vector3(0,1,0)).multiplyScalar(18) );
     
     coaster.camera.position = cube.position;
-    coaster.camera.rotation = cube.rotation;
+    coaster.camera.quaternion = cube.quaternion;
     
   }
   
